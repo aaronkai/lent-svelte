6950080f6f38a7f27da372640e01ba8f271c49b4
aaron <aaron.kai.hubbard@gmail.com>
2022-04-07 10:00:53 -0400

    add animation to link hover. fix typo.

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,7 +1,7 @@
 <header
 	class="flex lg:justify-center items-center p-6 bg-purple-200 border-b-2 border-purple-300 shadow"
 >
-	<a class="lg:text-center " href="/">
+	<a class="lg:text-center" href="/">
 		<h1 class="text-4xl font-bold   md:text-4xl lg:text-6xl">Meditations for Lent</h1>
 		<h2 class="italic text-purple-900 text-2xl pt-1  md:text-4xl lg:text-4xl">
 			by St. Thomas Aquinas
